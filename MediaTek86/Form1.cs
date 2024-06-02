a81cb1c3d4e8a20039d26ac1c6c71661c9f7bb72
Diegohbss <107653207+Diegohbss@users.noreply.github.com>
2024-06-02 13:21:52 +0200

    Changement en Form windows App

---
+++ MediaTek86/Form1.cs
@@ -0,0 +1,10 @@
+namespace MediaTek87
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MediaTek86/Program.cs
+++ MediaTek86/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MediaTek87;
+using System;
+using System.Windows.Forms;
 
 namespace MediaTek86
 {
     internal class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            LoginForm loginForm = new LoginForm();
+            if (loginForm.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new MainForm());
+            }
         }
     }
 }
